86ff9988a2cc49db16eaa10d34c6bcbd183b8faf
Matteo Zarola <matzar1808@gmail.com>
2020-07-14 11:09:53 +0200

    project

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+    'env': {
+        'commonjs': true,
+        'es2020': true,
+        'node': true
+    },
+    'extends': 'eslint:recommended',
+    'parserOptions': {
+        'ecmaVersion': 11
+    },
+    'rules': {
+        'indent': [
+            'error',
+            4
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'always'
+        ]
+    }
+};
