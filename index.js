89a10f63efedb6c4663065718e11dec5ab6a1af1
Woomy <woomy@woomy.be>
2020-08-19 15:22:06 +0200

    conf update

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const {
-    fiiMembers
+    fiiMembers, owners
 } = require('./config');
 
 require('dotenv').config();
@@ -7,7 +7,7 @@ const FIIB = require('./classes/client');
 const client = new FIIB({
     token: process.env.TOKEN,
     prefix: '&',
-    owners: ['743851266635071710', '462796153558138881'],
+    owners: owners,
     fiim: fiiMembers,
     commandPath: [`${__dirname}/commands`]
 });
@@ -15,19 +15,20 @@ client.on('ready', () => {
     client.user.setActivity('gérer la FII');
 });
 client.on('guildMemberAdd', (member) => {
-    if(client.raidmode) {
-        member.user.send('', {embed: {
-            title: 'Vous avez été expulsé!',
-            description: `Le serveur ${member.guild.name} est actuellement en Raidmode, revenez plus tard!`,
-            footer: {
-                text: 'RaidMode de FIIBOT',
-                iconURL: client.user.avatarURL({
-                    format: 'png'
-                })
-            },
-            color: 'RED'
-        }});
+    if (client.raidmode) {
+        member.user.send('', {
+            embed: {
+                title: 'Vous avez été expulsé!',
+                description: `Le serveur ${member.guild.name} est actuellement en Raidmode, revenez plus tard!`,
+                footer: {
+                    text: 'RaidMode de FIIBOT',
+                    iconURL: client.user.avatarURL({
+                        format: 'png'
+                    })
+                },
+                color: 'RED'
+            }
+        });
         member.kick('RAIDMODE');
-
     }
 });
