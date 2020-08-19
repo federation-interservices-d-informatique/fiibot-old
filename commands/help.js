2a38643c1e682a9d224bbe599b6489338a01235f
Woomy <woomy@woomy.be>
2020-08-19 16:24:46 +0200

    help commande added

---
+++ commands/help.js
@@ -0,0 +1,39 @@
+const Command = require('../classes/Command');
+module.exports = class helpcmd extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'help',
+            description: 'Obtenir l\'aide du bot',
+            hidden: true,
+            aliases: [
+                'aide'
+            ]
+        });
+    }
+    run(message){
+        let ebd = {
+            title: `Aide de ${this.client.user.username}`,
+            description: `Voici la liste des commandes que vous pouvez executer avec \`${this.client.options.prefix}commande\`.`,
+            fields: [],
+            color: 'RANDOM',
+            footer: {
+                icon_url: this.client.user.avatarURL({
+                    format: 'png'
+                }),
+                text: `Aide de ${this.client.user.username}`
+            }
+        };
+        this.client.commandManager.commands.forEach(cmd => {    
+            if(cmd.fiiOnly && !this.client.options.fiim.includes(message.author.id)) return;
+            if(cmd.guildOnly && message.channel.type === 'dm') return;
+            if(cmd.hidden && !this.client.isOwner(message.author)) return;
+            ebd.fields.push({
+                value: cmd.description,
+                name: cmd.name
+            });
+        });
+        message.channel.send('', {
+            embed: ebd
+        });
+    }
+};
