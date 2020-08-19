8144bd65b6cb940a0c8498d2d4d1709be57b6cec
Woomy4680-exe <matzar1808@gmail.com>
2020-08-19 14:34:16 +0200

    raidmode command checks

---
+++ commands/raidmode.js
@@ -0,0 +1,44 @@
+const Command = require('../classes/Command');
+module.exports = class raidmode extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'raidmode',
+            description: 'Lancer le raidmode',
+            aliases: [
+                'raid'
+            ],
+            fiiOnly: true
+        });
+    }
+    run(message) {
+        if(this.client.raidmode) {
+            message.channel.send('', { embed:  {
+                title: 'Raidmode',
+                description: 'Raidmode Désactivé!',
+                color: 'RED'
+            }});
+            this.client.raidmode = false;
+            this.client.channels.cache.get('705372306481872963').send('', {
+                embed: {
+                    title: 'Raidmode', 
+                    description: `Le raidmode a été désactivé par ${message.author.tag}`,
+                    color: 'RED'
+                }
+            });
+        } else {
+            message.channel.send('', { embed: {
+                title: 'Raidmode',
+                description: 'Raidmode Activé!',
+                color: 'RED'
+            }});
+            this.client.raidmode = true;
+            this.client.channels.cache.get('705372306481872963').send('', {
+                embed: {
+                    title: 'Raidmode', 
+                    description: `Le raidmode a été activé par ${message.author.tag}`,
+                    color: 'RED'
+                }
+            });
+        }
+    }
+};
