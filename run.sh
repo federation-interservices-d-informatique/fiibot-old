1c9c45ca16a753c7ebf5255a0c440ce144deac0e
Woomy <woomy@woomy.be>
2020-08-21 13:47:50 +0200

    run script updates

--- run.sh
+++ run.sh
@@ -1,5 +1,28 @@
-pm2 stop fiibot
-rm -rf node_modules
-npm install --production
-pm2 start --name fiibot index.js
-echo "Ok"
+#!/usr/bin/env/bash
+NAME="fiibot"
+if [[ "ps -ef | grep -i ${NAME} | grep -v grep" ]]; then
+    pm2 stop "${NAME}"
+fi
+if [[ ! -d "./node_modules" ]]; then
+    npm install --production
+    if [[ $? != 0 ]]; then
+        echo "Erreur lors de l'installation des modules!"
+        exit 1    
+    fi
+fi
+if [[ $1 == "reinstall" ]]; then
+    if [[ -d "./node_modules" ]]; then
+        rm -rf "./node_modules"
+        npm install --production
+        if [[ $? != 0 ]]; then
+            echo "Erreur lors de l'installation des modules!"
+            exit 1    
+        fi
+    else 
+        npm install --production
+        if [[ $? != 0 ]]; then
+            echo "Erreur lors de l'installation des modules!"
+            exit 1    
+        fi
+    fi
+fi
