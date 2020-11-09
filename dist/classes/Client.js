892ae5a47411c56cd647d6b11b35c52239251af1
Woomy <woomy@woomy.be>
2020-11-09 15:45:19 +0100

    add dist/to gitignore

--- dist/classes/Client.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const discordjs_moka_1 = require("discordjs-moka");
-class FIIClient extends discordjs_moka_1.mokaClient {
-}
-exports.default = FIIClient;
--- dist/classes/Command.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const discordjs_moka_1 = require("discordjs-moka");
-class fiiCommand extends discordjs_moka_1.Command {
-}
-exports.default = fiiCommand;
