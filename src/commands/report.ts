04b43d6853107b78b531d3c084179d33ceba8401
Woomy4680-exe <matzar1808@gmail.com>
2020-12-22 19:56:32 +0100

    ⬆️ Depeendencies upgrade

--- src/commands/report.ts
+++ src/commands/report.ts
@@ -1,43 +1,45 @@
 import { mokaMessage } from "discordjs-moka";
 import FIIClient from "../classes/Client";
-import Command from "../classes/Command"
-module.exports = class Report extends Command {
-    constructor(client: FIIClient) {
-        super(client, {
-            name: 'report',
-            description: 'Signaler un utilisateur',
-            aliases: [
-                'iléméchan'
-            ]
-        });
+import Command from "../classes/Command";
+module.exports = class Report extends (
+  Command
+) {
+  constructor(client: FIIClient) {
+    super(client, {
+      name: "report",
+      description: "Signaler un utilisateur",
+      aliases: ["iléméchan"],
+    });
+  }
+  async run(message: mokaMessage, args: string[]) {
+    if (!args[0] || !args[1]) {
+      message.channel.send(
+        `${message.author}! Veuillez préciser qui signaler!`
+      );
     }
-    async run(message: mokaMessage, args: string[]) {
-        if(!args[0] || !args[1]) {
-            message.channel.send(`${message.author}! Veuillez préciser qui signaler!`);
-        }
-        let user = this.client.users.resolve(args[0]); 
-        if(!user) {
-            return message.channel.send('Impossible de trouver l\'utilisateur!');
-        }
-        message.channel.send('', {
-            embed: {
-                title: `Report de ${user.username}`,
-                fields: [
-                    {
-                        name: 'Raison:',
-                        value:  `${args[1]}`
-                    }
-                ], 
-                timestamp: new Date(),
-                thumbnail: {
-                    url: `${user.avatarURL({format: 'png'})}`
-                },
-                footer: {
-                    text: `Report par ${message.author.username} depuis ${message.guild.name}`,
-                    icon_url: `${message.author.avatarURL({format: 'png'})}`
-                },
-                color: 'RANDOM'
-            }
-        });
+    let user = this.client.users.resolve(args[0]);
+    if (!user) {
+      return message.channel.send("Impossible de trouver l'utilisateur!");
     }
+    message.channel.send("", {
+      embed: {
+        title: `Report de ${user.username}`,
+        fields: [
+          {
+            name: "Raison:",
+            value: `${args[1]}`,
+          },
+        ],
+        timestamp: new Date(),
+        thumbnail: {
+          url: `${user.avatarURL({ format: "png" })}`,
+        },
+        footer: {
+          text: `Report par ${message.author.username} depuis ${message.guild.name}`,
+          icon_url: `${message.author.avatarURL({ format: "png" })}`,
+        },
+        color: "RANDOM",
+      },
+    });
+  }
 };
