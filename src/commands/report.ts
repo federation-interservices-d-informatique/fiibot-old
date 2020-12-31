9a238758ebe9be83446b49f1285ebe4c1509ecb9
Woomy4680-exe <matzar1808@gmail.com>
2020-12-31 12:23:50 +0100

    ✨ Send reports in other channel

--- src/commands/report.ts
+++ src/commands/report.ts
@@ -1,3 +1,4 @@
+import { TextChannel } from "discord.js";
 import { mokaMessage } from "discordjs-moka";
 import FIIClient from "../classes/Client";
 import Command from "../classes/Command";
@@ -18,30 +19,50 @@ module.exports = class Report extends (
       );
       return;
     }
-    let user = await this.client.users.fetch(args[0]) || message.mentions.users.first();
+    let user = message.mentions.users.first() || (await this.client.users.fetch(args[0]));
     if (!user) {
       return message.channel.send("Impossible de trouver l'utilisateur!");
     }
-    const reason = args.slice(1).join(' ')
-    message.channel.send("", {
-      embed: {
-        title: `Report de ${user.username}`,
-        fields: [
-          {
-            name: "Raison:",
-            value: `${reason}`,
+    const reason = args.slice(1).join(" ");
+    const chan = await this.client.channels.fetch(
+      this.client.fii.critLogChan,
+      true,
+      true
+    ) as TextChannel;
+    if (chan) {
+      chan.send("", {
+        embed: {
+          title: `Report de ${user.username}`,
+          fields: [
+            {
+              name: "Raison:",
+              value: `${reason}`,
+            },
+            {
+              name: 'Lien du dernier message:', 
+              value: `[Lien](https://discord.com/channels/${message.guild.id}/${message.channel.id}/${message.channel.lastMessageID})`
+            }
+          ],
+          timestamp: new Date(),
+          thumbnail: {
+            url: `${user.avatarURL({ format: "png" })}`,
           },
-        ],
-        timestamp: new Date(),
-        thumbnail: {
-          url: `${user.avatarURL({ format: "png" })}`,
+          footer: {
+            text: `Report par ${message.author.username} depuis ${message.guild.name}`,
+            icon_url: `${message.author.avatarURL({ format: "png" })}`,
+          },
+          color: "RANDOM",
         },
-        footer: {
-          text: `Report par ${message.author.username} depuis ${message.guild.name}`,
-          icon_url: `${message.author.avatarURL({ format: "png" })}`,
-        },
-        color: "RANDOM",
-      },
-    });
+      });
+      message.channel.send('', {embed: {
+        title: 'Merci de votre signalement',
+        description: 'Votre signalement a bien été envoyé!',
+        color: 'GREEN'
+      }})
+    } else {
+      message.channel.send(
+        "Oups! On dirait que il n'existe plus de salon ou je puisse envoyer les reports!"
+      );
+    }
   }
 };
