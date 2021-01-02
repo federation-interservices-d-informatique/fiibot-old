daedf81f5f1d711c510bcfc878ae1cbffdc7051b
Woomy <woomy@woomy.be>
2021-01-02 11:33:28 +0100

    ✨ Add IDreset command

---
+++ src/commands/id/idreset.ts
@@ -0,0 +1,33 @@
+import { mokaMessage } from "discordjs-moka";
+import FIIClient from "../../classes/Client";
+import fiiCommand from "../../classes/Command";
+
+module.exports = class extends fiiCommand {
+    constructor(client: FIIClient) {
+        super(client, {
+            name: 'idreset',
+            description: 'Supprimer un id FII',
+            ownerOnly: true,
+            guildOnly: true,
+            usage: `idreset {username} {id}`
+        })
+    }
+    async run(message: mokaMessage, args: string[]) {
+        if(!['743851266635071710', '459065939766542368'].includes(message.author.id)) {
+            message.channel.send('Vous ne pouvez pas faire ça!');
+        }
+        if(!args[0] || !args[1]) return;
+        if(!this.client.idb.has(args[0])) {
+            message.channel.send(`L'utilisateur ${args[0]} n'existe pas!`);
+        } else {
+            this.client.idb.delete(args[0]);
+        }
+
+        message.channel.send('OK!');
+        let users: string[] = this.client.idb.get('registredusers');
+        users = users.filter((u) => {
+            u != args[1]
+        });
+        this.client.idb.set('registredusers', users);
+    }
+}
