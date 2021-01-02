855bb9421f7230125b285c2c5a79601236ffb0e2
Woomy4680-exe <matzar1808@gmail.com>
2021-01-02 17:25:57 +0100

    ✨ Add unautoriseinvites command

---
+++ src/commands/config/unautoriseinvites.ts
@@ -0,0 +1,27 @@
+import { mokaMessage } from "discordjs-moka";
+import FIIClient from "../../classes/Client";
+import fiiCommand from "../../classes/Command";
+
+module.exports = class extends fiiCommand {
+    constructor(client: FIIClient) {
+        super(client, {
+            name: 'unautoriseinvites',
+            description: 'Interdire les invites dans un salon / sur le serveur',
+            userPermissions: ['ADMINISTRATOR']
+        })
+    }
+    async run(message: mokaMessage) {
+        const autorised = message.guild.settings.get('autoriseinvites') || false;
+        let autorisedChans: Array<string> = message.guild.settings.get('autorisedinviteschans') || new Array();
+        if(!message.mentions.channels.first()) {
+            message.guild.settings.set('autoriseinvites', autorised ? false : true);
+            message.channel.send(`Les invitations sont maintenant ${autorised ? 'interdites' : 'autorisées'}`);
+            return
+        }
+        const todisable = new Array();
+        message.mentions.channels.forEach(c => todisable.push(c.id))
+        autorisedChans = autorisedChans.filter(c => todisable.includes(c) == false)
+        message.guild.settings.set('autorisedinviteschans', autorisedChans);
+        message.channel.send(`Les salons ${autorisedChans.map(c => `<#${c}>`).toString()} sont encore ignorés!`)
+    }
+}
