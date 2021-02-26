886a94e33246b40f0ec425c444a850b9d939e34b
Woomy4680-exe <matzar1808@gmail.com>
2021-02-26 21:08:39 +0100

    ♻️ Use constants instead of hardcoding each regex

---
+++ src/Constants/Regex.ts
@@ -0,0 +1,2 @@
+export const INVITATION_REGEX = /(https:\/\/|http:\/\/|)?(www)?discord.(com\/invite|gg)\/[0-Z]{1,20}/gim
+export const ID_REGEX = /FII-(LPT|CLI|MIM|HUB|ADP)-[0-9]{6}-[0-9]{10}-FII/gim;
